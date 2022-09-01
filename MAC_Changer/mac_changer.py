cfea94d4f9e6a1d57fb38f5ff7a78bfff9e3d6bd
Abhishek Kumar <abhishek.ak996@gmail.com>
2022-09-01 13:25:32 -0700

    Commit

--- MAC_Changer/mac_changer.py
+++ MAC_Changer/mac_changer.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
 
 import subprocess
-import optparse
+import argparse
 
-parser = optparse.OptionParser()
+parser = argparse.ArgumentParser()
 
-interface = input("interface :   ")
-new_mac = input("new MAC :   ")
+
+
+parser.add_argument("-i", "--interface", dest="interface", help="The MAC Address of which interface you want to change")
+parser.add_argument("-m", "--mac", dest="new_mac", help="The MAC Address you want to change")
+args = parser.parse_args()
+
+# interface = input("interface :   ")
+interface = args.interface
+
+# new_mac = input("new MAC :   ")
+new_mac = args.new_mac
 
 
 print("[+] Your old Mac_changer is here ")
-print()
-print()
-print()
+
 subprocess.run("ifconfig ", shell=True, check=False)
 
 print("[+] Changing MAC Address for " + interface + " to "  + new_mac)
