e74e43bf7a53a1cc5659aa2f087756a7fb6c9bcd
Abhiak996 <abhishek.ak996@gmail.com>
2022-09-01 17:04:29 -0700

    commit

--- MAC_Changer/mac_changer.py
+++ MAC_Changer/mac_changer.py
@@ -2,18 +2,19 @@
 
 import subprocess
 import argparse
+from time import sleep
 
 parser = argparse.ArgumentParser()
-
-
 
 parser.add_argument("-i", "--interface", dest="interface", help="The MAC Address of which interface you want to change")
 parser.add_argument("-m", "--mac", dest="new_mac", help="The MAC Address you want to change")
 args = parser.parse_args()
 
-interface = args.interface
 
+interface = args.interface
 new_mac = args.new_mac
+
+
 
 
 print("[+] Your old Mac_changer is here ")
@@ -27,3 +28,7 @@ print("[+] Changing MAC Address for " + interface + " to "  + new_mac)
 subprocess.run("ifconfig "+ interface + " down", shell=True, check=False)
 subprocess.run("ifconfig "+ interface + " hw ether "+new_mac, shell=True, check=False)
 subprocess.run("ifconfig "+ interface + " up", shell=True, check=False)
+
+print("\n[+] Running Command ifconfig to check the new MAC Address")
+sleep(2)
+subprocess.run(["ifconfig"])
