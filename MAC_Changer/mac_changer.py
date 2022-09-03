9033323c19a889802c2ae4e3406380a29a9acea2
Abhiak996 <abhishek.ak996@gmail.com>
2022-09-03 07:09:55 +0530

    commit

--- MAC_Changer/mac_changer.py
+++ MAC_Changer/mac_changer.py
@@ -3,11 +3,21 @@
 import subprocess
 import argparse
 from time import sleep
+import re
+
+
+def current_mac(interface):
+	ifconfig_output = subprocess.check_output(["ifconfig", interface])
+
+	mac = re.search(r"(\w{2}[:-]){5}\w{2}", str(ifconfig_output))
+	return mac.group(0)
+	
+
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-i", "--interface", dest="interface", help="The MAC Address of which interface you want to change")
-parser.add_argument("-m", "--mac", dest="new_mac", help="The MAC Address you want to change")
+parser.add_argument("-m", "--mac", dest="new_mac", help="The Updated MAC Address you want")
 args = parser.parse_args()
 
 if not args.interface:
@@ -24,9 +34,8 @@ elif not new_mac:
 	parser.error("[-] Specify a new MAC")
 
 
-print("[+] Your old Mac_changer is here ")
+print("[+] Your old Mac_changer is here :   {}\n".format(current_mac(interface)))
 
-subprocess.run("ifconfig ", shell=True, check=False)
 
 print("[+] Changing MAC Address for " + interface + " to "  + new_mac)
 
@@ -36,6 +45,13 @@ subprocess.run("ifconfig "+ interface + " down", shell=True, check=False)
 subprocess.run("ifconfig "+ interface + " hw ether "+new_mac, shell=True, check=False)
 subprocess.run("ifconfig "+ interface + " up", shell=True, check=False)
 
-print("\n[+] Running Command ifconfig to check the new MAC Address")
+
+print("\n[+] Running Command ifconfig to check the new MAC Address\n")
 sleep(2)
-subprocess.run(["ifconfig"])
+
+mac = current_mac(interface)
+
+if mac:
+	print("The New MAC Address is {}".format(mac))
+else:
+	print("[-] MAC address is not here")
