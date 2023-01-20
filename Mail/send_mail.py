05eff603039118a8b1ca16b392d5f6efca7d7018
Abhishek Kumar <78052345+Abhiak99@users.noreply.github.com>
2023-01-21 00:58:35 +0530

    Create send_mail.py

---
+++ Mail/send_mail.py
@@ -0,0 +1,13 @@
+import smtplib
+
+
+
+def send_mail(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
+   
+  
+send_mail(email, password, message)
